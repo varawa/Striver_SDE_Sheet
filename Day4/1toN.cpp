db4b6f4d65b7f2ca09a714dd8c69df5933ec0117
varawa <142249631+varawa@users.noreply.github.com>
2024-03-06 23:36:39 +0530

    Print 1 to N using recursion .

---
+++ Day4/1toN.cpp
@@ -0,0 +1,12 @@
+vector<int> printNos(int x) {
+    if(x == 1){
+        vector<int> v ;
+        v.push_back(1) ;
+        return v ;
+    }
+
+    vector<int> v = printNos(x-1) ; 
+    v.push_back(x) ;
+
+    return v ;
+}
