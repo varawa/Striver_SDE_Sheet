e312c490854198f17defe2983a3a512cc4f348ce
varawa <142249631+varawa@users.noreply.github.com>
2024-04-13 21:12:28 +0530

    Remove duplicates in array

---
+++ Array/Easy/Duplicates.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+#include<vector>
+
+using namespace std ;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& arr) {
+        int i = 0;
+        for (int j = 1; j < arr.size(); j++) {
+            if (arr[i] != arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+  }
+  return i + 1;
+    }
+};
