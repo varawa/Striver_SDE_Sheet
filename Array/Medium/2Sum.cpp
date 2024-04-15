6dc7563bf9d8db6909624f68435c260f8fe8fd46
varawa <142249631+varawa@users.noreply.github.com>
2024-04-15 20:44:50 +0530

    Sum equals target

--- Array/Medium/2Sum.cpp
+++ Array/Medium/2Sum.cpp
@@ -8,14 +8,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        vector<int> res;
-        for(int i = 0;i < nums.size();i++){
-            for(int j = i+1;j<nums.size();j++){
-                if(nums[i]+nums[j] == target){
-                    return {i,j};
+        vector<int> res ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            for(int j = i + 1 ; j < nums.size() ; j++){
+                if(nums[i] + nums[j] == target){
+                    return {i , j} ;
                 }
             }    
         }
-        return {};
+        return res;
     }
 };
