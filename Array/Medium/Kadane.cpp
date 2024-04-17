7a1272bb191f0ab506000f1686ec9c055dc03e33
varawa <142249631+varawa@users.noreply.github.com>
2024-04-17 22:35:40 +0530

    Kadane's algorithm

---
+++ Array/Medium/Kadane.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+#include<vector>
+
+using namespace std ;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size() ;
+        int sum = 0 , maxi = INT_MIN ;
+        for(int i = 0 ; i < n ; i++){
+            sum += nums[i] ; 
+            if(sum > maxi){
+                maxi = sum ;
+            }
+            if(sum < 0){
+                sum = 0 ;
+            }
+        }
+        return maxi ;
+    }
+};
