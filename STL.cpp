1c5f049c7fd87c9a105ae091c48b722fcab8615d
varawa <142249631+varawa@users.noreply.github.com>
2024-06-10 12:40:23 +0530

    MultiSet

--- STL.cpp
+++ STL.cpp
@@ -263,3 +263,24 @@ void Set(){
     auto it = s.upper_bound(3) ;
     
 }
+
+void Multiset(){
+    //Everything is same as set .
+    //Only stores duplicate elements .
+
+    multiset<int>ms ;
+
+    ms.insert(1) ;      //{1}
+    ms.insert(4) ;      //{1 , 4}
+    ms.insert(4) ;      //{1 , 4 , 4}
+
+    ms.erase(1) ;       //All 1's erased .
+
+    int cnt = ms.count(1) ;
+
+    //Only a single one erased .
+    ms.erase(ms.find(1)) ;
+
+    //Rest all functions same as set .
+
+}
