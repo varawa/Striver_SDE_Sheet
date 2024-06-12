f3c23659983692251b1f5931046bfc802698984f
varawa <142249631+varawa@users.noreply.github.com>
2024-06-12 10:14:16 +0530

    Creation of doubly linked list .

---
+++ LinkedList/DoublyLL/Creation.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    Node* prev;
+    int data;
+    Node* next;
+ 
+    Node()
+    { 
+        prev = NULL; 
+        data = 0;
+        next = NULL;
+    }
+    
+    Node(int value)
+    { 
+        prev = NULL; 
+        data = value;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*
+class Node{
+public:
+    Node* prev;
+    int data;
+    Node* next;
+ 
+    Node()
+    { 
+        prev = NULL; 
+        data = 0;
+        next = NULL;
+    }
+    
+    Node(int value)
+    { 
+        prev = NULL; 
+        data = value;
+        next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructDLL(vector<int>& arr) {
+        // code here
+        Node* head = new Node(arr[0]) ;
+        Node* temp = head ;
+        
+        for(int i = 1 ; i < arr.size() ; i++){
+            temp->next = new Node(arr[i]) ;
+            Node* temp1 = temp ;
+            temp = temp->next ;
+            temp->prev = temp1 ;
+        }
+        
+        
+        return head ;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        Node* ans = obj.constructDLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
