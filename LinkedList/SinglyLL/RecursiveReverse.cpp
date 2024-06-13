cf0176fb1972f54371b16befd557c3256bd9ef17
varawa <142249631+varawa@users.noreply.github.com>
2024-06-13 10:32:15 +0530

    Recursive linked list reversal .

---
+++ LinkedList/SinglyLL/RecursiveReverse.cpp
@@ -0,0 +1,27 @@
+
+ //Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr)
+            return head ;
+        
+        ListNode* rev = reverseList(head->next) ;
+        
+        ListNode* temp = head->next ;
+            
+        temp->next = head ;
+        head->next = nullptr ;
+        
+        return rev ;
+        
+    }
+};
