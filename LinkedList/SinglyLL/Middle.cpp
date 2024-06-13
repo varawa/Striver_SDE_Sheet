edd6032e2217fc7525ed07a75d114758b7e958fc
varawa <142249631+varawa@users.noreply.github.com>
2024-06-13 13:16:55 +0530

    Middle element hare tortoise

--- LinkedList/SinglyLL/Middle.cpp
+++ LinkedList/SinglyLL/Middle.cpp
@@ -1,33 +1,23 @@
 
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
+ //Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
       ListNode(int x) : val(x), next(nullptr) {}
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
-
+ 
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head ;
-        int count = 0 ;
+        ListNode* fast = head ;
+        ListNode* slow = head ;
 
-        if(head->next == nullptr)
-            return head ;
-
-        while(temp != nullptr){
-            temp = temp->next ;
-            count++ ;
+        while(fast != nullptr && fast->next != nullptr){
+            fast = fast->next->next ;
+            slow = slow->next ;
         }
-        temp = head ;
-        int mid = count/2 ;
-        
-        for(int i = 0 ; i < mid ; i++){
-            temp = temp->next ;
-        }
-        head = temp ;
-
-        return head ;
+        return slow ;
     }
 };
