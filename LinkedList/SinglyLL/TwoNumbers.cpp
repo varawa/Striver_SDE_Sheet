020ccdf6e9bcd82f5527a2d379fa456c10ed43eb
varawa <142249631+varawa@users.noreply.github.com>
2024-06-24 10:10:24 +0530

    add two numbers in LL

--- LinkedList/SinglyLL/TwoNumbers.cpp
+++ LinkedList/SinglyLL/TwoNumbers.cpp
@@ -1,56 +1,39 @@
 
-// * Definition for singly-linked list.
-struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
+ // Definition for singly-linked list.
+ struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
  
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode() ;
+        ListNode* temp = dummy ;
+
+        int sum = 0 ; 
         int carry = 0 ;
-        ListNode* head = new ListNode() ;
-        ListNode* ans = head ;
         
-        ListNode* prev = nullptr ;
-
-        while(l1 || l2){
-            if(l1 && l2){
-                int sum = l1->val + l2->val ;
-                ans->val = (l1->val + l2->val + carry)%10 ;
-                carry = (sum + carry)/10 ;
-                l1 = l1->next ;
-                l2 = l2->next ;
-            }
-            else if(l1){
-                int sum = l1->val ;
-                ans->val = (l1->val + carry)%10 ;
-                carry = (sum + carry)/10 ;
+        while(l1 || l2 || carry){
+            sum = carry ;
+            
+            if(l1){
+                sum += l1->val ;
                 l1 = l1->next ;
             }
-            else{
-                int sum = l2->val ;
-                ans->val = (l2->val + carry)%10 ;
-                carry = (sum + carry)/10 ;
+            
+            if(l2){
+                sum += l2->val ;
                 l2 = l2->next ;
             }
-
-            ans->next = new ListNode() ;
-            prev = ans ;
-            ans = ans->next ;
-
+            
+            dummy->next = new ListNode(sum%10) ;
+            carry = sum/10 ;
+            dummy = dummy->next ;
         }
-
-        if(carry){
-            ans->val = carry ;
-        }else{
-            prev->next = nullptr ;
-        }
-
-        return head ;
-
+        return temp->next ;
     }
 };
