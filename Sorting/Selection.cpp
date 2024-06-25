99265843b4b1a84e5c0faa74f7e6ca67ef5f9a5d
varawa <142249631+varawa@users.noreply.github.com>
2024-06-25 21:52:46 +0530

    selection sort

--- Sorting/Selection.cpp
+++ Sorting/Selection.cpp
@@ -1,22 +1,75 @@
-#include<iostream>
-#include<math.h>
-#include<vector>
-#include<limits.h>
-
-using namespace std ;
-
-void selectionSort(vector<int>&arr) {
-    // Write your code here.
-    int i, j, min_idx;
-    int n = arr.size() ;
-
-    for (i = 0; i < n - 1; i++) {
-        min_idx = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
-        }
-        if (min_idx != i)
-            swap(arr[min_idx], arr[i]);
-    }
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    int select(int arr[], int i , int n)
+    {
+        int temp = INT_MAX ;
+        int index = i ;
+        
+        for(int j = i ; j < n ; j++){
+            if(temp > arr[j]){
+                temp = arr[j] ;
+                index = j ;
+            }
+        }
+        return index ;
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+       for(int i = 0 ; i < n ; i++){
+            int k = select(arr , i , n) ;
+            
+            int temp = arr[i] ;
+            arr[i] = arr[k] ;
+            arr[k] = temp ;
+            
+       }
+    }
+};
+
+//{ Driver Code Starts.
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    Solution ob;  
+    ob.selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
