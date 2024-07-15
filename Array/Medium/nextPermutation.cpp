426aad5b01ed6fd5a0d2ff13e099e67d3b87e653
varawa <142249631+varawa@users.noreply.github.com>
2024-07-15 22:07:10 +0530

    better approach

--- Array/Medium/nextPermutation.cpp
+++ Array/Medium/nextPermutation.cpp
@@ -6,29 +6,38 @@ class Solution {
 public:
     void nextPermutation(vector<int>& A) {
         if(A.size() == 0 || A.size() == 1)  return ;
-        
-        bool flag = false ;
 
         int start = A.size() - 2 ;
         int end = A.size() - 1 ;
 
+        int ind = -1 ;
+
         while(start >= 0){
-            int max = INT_MAX ;
-            int check = A[start] ;
-            for(int j = start ; j < A.size() ; j++){
-                if(A[j] < max && A[j] > check){
-                    max = A[j] ;
-                    int temp = A[j] ;
-                    A[j] = A[start] ;
-                    A[start] = temp ;
-                    flag = true ;
-                }
-                sort(A.begin() + start + 1 , A.end()) ;
+            if(A[start] < A[end]){
+                ind = start ;
+                break ;
             }
-            if(flag == true)    return ;
             start-- ;
+            end-- ;
         }
-        if(flag == false)   sort(A.begin() , A.end()) ;
+
+        if(ind == -1){
+            reverse(A.begin() , A.end()) ;
+            return ;
+        }  
+
+        int max = INT_MAX ; 
+        int n = -1 ;
+
+        for(int i = A.size() - 1 ; i > ind ; i--){
+            if(A[i] > A[ind] && A[i] < max){
+                max = A[i] ;
+                n = i ;
+            }
+        }
+        swap(A[ind] , A[n]) ;
+        
+        reverse(A.begin() + ind + 1 , A.end()) ;
     }
 
 };
