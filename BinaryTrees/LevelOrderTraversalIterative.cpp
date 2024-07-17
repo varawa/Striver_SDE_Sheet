ecbcbc5b03a7370db1e4518a46fe307724ecd7af
varawa <142249631+varawa@users.noreply.github.com>
2024-07-17 11:05:17 +0530

    iterative level order traversal

---
+++ BinaryTrees/LevelOrderTraversalIterative.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res ;
+        if(root == nullptr) return res ;
+
+        queue<TreeNode*>q ;
+        q.push(root) ;
+
+        while(!q.empty()){
+            vector<int>line ;
+            int s = q.size() ;
+            for(int i = 0 ; i < s ; i++){
+                TreeNode* temp = q.front() ;
+                q.pop() ;
+                line.push_back(temp->val) ;
+                if(temp->left)  q.push(temp->left) ; 
+                if(temp->right) q.push(temp->right) ;
+            }
+            res.push_back(line) ;
+        }
+        return res ;
+    }
+
+};
