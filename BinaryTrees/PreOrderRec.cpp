77e4a115f8132bdfe8e75e22f62ddaa1b1b13643
varawa <142249631+varawa@users.noreply.github.com>
2024-07-17 10:20:39 +0530

    rescursive pre order

---
+++ BinaryTrees/PreOrderRec.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res ;
+        preorder(root , res) ;
+        return res ;
+    }
+
+    void preorder(TreeNode* root , vector<int>& res) { 
+        if(root == nullptr) return ;
+        res.push_back(root->val) ;
+        
+        if(root->left)
+            preorder(root->left , res) ;
+        
+        if(root->right)
+            preorder(root->right , res) ;
+    }
+};
