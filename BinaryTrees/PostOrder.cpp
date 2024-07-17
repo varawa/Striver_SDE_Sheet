e254975a830fb7e4d1a24fe3d257b9ff7a345e72
varawa <142249631+varawa@users.noreply.github.com>
2024-07-17 10:35:19 +0530

    post order recursive

---
+++ BinaryTrees/PostOrder.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res ;
+        postorder(root , res) ;
+        return res ;
+    }
+
+    void postorder(TreeNode* root , vector<int>& res){
+        if(root == nullptr) return ;
+        postorder(root->left , res) ;
+        postorder(root->right , res) ;
+        res.push_back(root->val) ;
+    }
+};
