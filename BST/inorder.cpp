21d2d3a977a124c0bab761245b9cba62b7dc4bb5
varawa <142249631+varawa@users.noreply.github.com>
2024-07-24 23:16:00 +0530

    basic

---
+++ BST/inorder.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// {Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    bool isBSTTraversal(vector<int>& arr) {
+        int prev = -1 ;
+        for(auto item : arr){
+            if(prev == item || prev > item)    return false ;
+            prev = item ;
+        }
+        return true ;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++) {
+            int k;
+            cin >> k;
+            nums[i] = k;
+        }
+
+        Solution ob;
+        bool res = ob.isBSTTraversal(nums);
+        if (res)
+            cout << "True";
+        else
+            cout << "False";
+
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
