01a94381cb6c5939118e8d84296044f35b3207f5
varawa <142249631+varawa@users.noreply.github.com>
2024-07-26 22:42:41 +0530

    two sum with sorted array

---
+++ Array/Medium/LC167.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res ;
+
+        int start = 0 ;
+        int end = numbers.size() - 1 ;
+
+        while(start <= end){
+            if(numbers[start] + numbers[end] == target){
+                res.push_back(start + 1) ;
+                res.push_back(end + 1) ;
+                return res ;
+            }else if(numbers[start] + numbers[end] >= target)
+                end-- ;
+            else
+                start++ ;
+        }
+        return res ;
+    }
+};
