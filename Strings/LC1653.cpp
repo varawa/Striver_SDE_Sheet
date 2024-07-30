4bc38d69f1760974e984d0b3db60596379fb2c28
varawa <142249631+varawa@users.noreply.github.com>
2024-07-30 09:35:03 +0530

    minimum deletions

---
+++ Strings/LC1653.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.length();
+        int bCount = 0, aCount = 0;
+        int deletions = 0;
+        
+        for (char c : s) {
+            if (c == 'b') {
+                bCount++;
+            } else {
+                deletions++;
+                deletions = min(deletions, bCount);
+            }
+        }
+        
+        return deletions;
+    }
+};
