906955ec042f32ed3b215764e7728d3e51189340
varawa <142249631+varawa@users.noreply.github.com>
2024-09-01 10:38:42 +0530

    daily challenge

---
+++ Array/Easy/LC2022.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+class Solution {
+public:
+    static vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        const int sz=original.size();
+        if (sz!=m*n) return {};
+        vector<vector<int>> ans(m);
+        for(int i=0; i<m; i++){
+            ans[i].assign(original.begin()+i*n, original.begin()+(i+1)*n);
+        }
+        return ans;
+    }
+};
+
