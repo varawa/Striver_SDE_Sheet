862ee471166ae482f2d2a692596e51d9d91c5093
varawa <142249631+varawa@users.noreply.github.com>
2024-10-09 15:15:33 +0530

    changes

--- DCC_Leetcode/Oct/LC921.cpp
+++ DCC_Leetcode/Oct/LC921.cpp
@@ -1,25 +1,18 @@
-#include<bits/stdc++.h>
-
-using namespace std ;
-
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char>stk ;
-
-        for(auto c : s){
-            if(!stk.empty() && stk.top() == '(' && c == ')'){
-                stk.pop() ;
-            }else{
-                stk.push(c) ;
-            }
-        }
-        int ans = 0 ; 
-        while(!stk.empty()){
-            ans++ ;
-            stk.pop() ;
+        int openparen = 0 ;
+        int closeparen = 0 ;
+        
+        for(auto ch : s){
+            if(ch=='(')
+                openparen++;
+            else if(ch==')' && openparen>0)
+                openparen--;
+            else
+                closeparen++;
         }
 
-        return ans ;
+        return openparen + closeparen ;
     }
 };
